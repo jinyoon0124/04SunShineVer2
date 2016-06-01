8e4e84871799264b9f38826277207632712215d8
Jin Yoon <jinyoon0124@gmail.com>
2016-05-31 22:30:36 -0400

    Lesson7 Empty View added

--- app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
@@ -139,6 +139,8 @@ public class ForecastFragment extends Fragment implements LoaderManager.LoaderCa
 
         // Get a reference to the ListView, and attach this adapter to it.
         mListView = (ListView) rootView.findViewById(R.id.listview_forecast);
+        //EmptyView added Ver2
+        mListView.setEmptyView(rootView.findViewById(android.R.id.empty));
         mListView.setAdapter(mForecastAdapter);
         // We'll call our MainActivity
         mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
