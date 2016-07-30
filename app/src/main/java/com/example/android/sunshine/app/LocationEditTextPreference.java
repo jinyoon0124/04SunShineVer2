1edc66cc4d19534cbb78629e5d5f0b2a036c2425
Jin Yoon <jinyoon0124@gmail.com>
2016-07-30 04:13:23 -0400

    Lesson 12 Place Picker Widget part 1 (Layout, Google Services Availability Check, drawables)

--- app/src/main/java/com/example/android/sunshine/app/LocationEditTextPreference.java
+++ app/src/main/java/com/example/android/sunshine/app/LocationEditTextPreference.java
@@ -24,12 +24,19 @@ import android.preference.EditTextPreference;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 
 public class LocationEditTextPreference extends EditTextPreference {
     static final private int DEFAULT_MINIMUM_LOCATION_LENGTH = 2;
     private int mMinLength;
+
 
     public LocationEditTextPreference(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -42,8 +49,28 @@ public class LocationEditTextPreference extends EditTextPreference {
         } finally {
             a.recycle();
         }
+
+        //Google Play Service available check for Places API
+        GoogleApiAvailability apiAvailability=GoogleApiAvailability.getInstance();
+        int resultCode=apiAvailability.isGooglePlayServicesAvailable(getContext());
+        if(resultCode== ConnectionResult.SUCCESS){
+            setWidgetLayoutResource(R.layout.pref_current_location);
+        }
     }
 
+    @Override
+    protected View onCreateView(ViewGroup parent) {
+        View view =  super.onCreateView(parent);
+        View currentLocation = view.findViewById(R.id.current_location);
+        currentLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getContext(), "WOO!", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        return view;
+    }
 
     @Override
     protected void showDialog(Bundle state) {
