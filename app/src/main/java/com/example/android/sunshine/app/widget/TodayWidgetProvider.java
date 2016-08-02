539f9d7dd867bcc26215acaf1f587ea9ed6bd1ef
Jin Yoon <jinyoon0124@gmail.com>
2016-08-02 19:39:04 -0400

    Lesson 13 Widgets Widgets I: Static widget added (Widget layout, provider Info (meta data), provider java, manifest)

---
+++ app/src/main/java/com/example/android/sunshine/app/widget/TodayWidgetProvider.java
@@ -0,0 +1,27 @@
+package com.example.android.sunshine.app.widget;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+import com.example.android.sunshine.app.MainActivity;
+import com.example.android.sunshine.app.R;
+
+/**
+ * Created by Jin Yoon on 8/2/2016.
+ */
+public class TodayWidgetProvider extends AppWidgetProvider {
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        for(int appWidgetId: appWidgetIds){
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_today);
+            Intent intent = new Intent(context, MainActivity.class);
+            PendingIntent pendingIntent=PendingIntent.getActivity(context,0,intent,0);
+            views.setOnClickPendingIntent(R.id.widget, pendingIntent);
+            appWidgetManager.updateAppWidget(appWidgetId, views);
+        }
+    }
+}
